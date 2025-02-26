8d26177d7d30d0e53d9bf1b70d00614f603113cf
HoromiUzumaki <ksushakrotova16@yandex.ru>
2025-02-26 22:52:07 +0300

    Сессия_2
    Реализованы методы GET и POST запросов:
    3. Получение списка складов
    4. Отправка данных о товаре

---
+++ WebApplication1/WebApplication1/Controllers/WarehouseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+using WebApplication1.NewFolder1;
+
+namespace WebApplication1.Controllers
+{
+    public class WarehouseController : ApiController
+    {
+        private Складской_учетEntities db = new Складской_учетEntities();
+        // GET: api/Склады
+        [ResponseType(typeof(List<Response_Warehouse>))]
+
+        public IHttpActionResult GetWarehouse()
+        {
+            return Ok(db.Warehouse.ToList().ConvertAll(p => new Response_Warehouse(p)));
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Response_Warehouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.NewFolder1;
+
+namespace WebApplication1.Models
+{
+    public class Response_Warehouse
+    {
+        public Response_Warehouse(Warehouse склады)
+        {
+            IdWarehouse = склады.IdWarehouse;
+            Title = склады.Title;
+            Address = склады.Address;
+            IdWarehouseType = склады.IdWarehouseType;
+            IdStorageArea = склады.IdStorageArea;
+        }
+        public int IdWarehouse { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public int IdWarehouseType { get; set; }
+        public int IdStorageArea { get; set; }
+    }
+}
