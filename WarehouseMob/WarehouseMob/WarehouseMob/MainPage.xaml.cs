d8ce6bd6fd6d843a2f03a0a694aa82e01521d3d7
HoromiUzumaki <ksushakrotova16@yandex.ru>
2025-03-12 11:58:50 +0300

    Сессия_5
    Мобильное приложение предназначено для роли кладовщика, но просмотр данных могут осуществлять и другие роли
    Функции мобильного приложения:
    1. Двухфакторная аутентификация
    2. Реализовано меню со следующими фрагментами:
    а. Список складов и их местоположение
    б. Список товаров по складам
    в. Сканирование штрихкода(QR кода) для проверки товара
    г. Профиль с пропуском
    д. Окно справки с описанием реализованных функций.

---
+++ WarehouseMob/WarehouseMob/WarehouseMob/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace WarehouseMob
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly AuthService _authService;
+        public MainPage()
+        {
+            InitializeComponent();
+            _authService = new AuthService(App.Database);
+        }
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            var email = EmailEntry.Text;
+            var password = PasswordEntry.Text;
+
+            try
+            {
+                // Первый этап: проверка почты и пароля
+                var пользователь = await _authService.AuthenticateAsync(email, password);
+
+                // Второй этап: двухфакторная аутентификация (ввод роли)
+                var роль = await DisplayPromptAsync("Двухфакторная аутентификация", "Введите вашу роль:");
+
+                if (_authService.VerifyRole(пользователь, роль))
+                {
+                    // Успешная аутентификация
+                    App.CurrentUser = пользователь;
+                    await DisplayAlert("Успех", "Аутентификация прошла успешно!", "OK");
+                    await Navigation.PushAsync(new TabbedPage1()); // Переход на главную страницу
+                }
+                else
+                {
+                    await DisplayAlert("Ошибка", "Неверная роль.", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ошибка", ex.Message, "OK");
+            }
+        }
+    }
+}
