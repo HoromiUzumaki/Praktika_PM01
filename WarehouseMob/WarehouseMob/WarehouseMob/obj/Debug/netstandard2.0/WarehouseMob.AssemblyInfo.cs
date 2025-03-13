ca10bb345ccb2c0be646a47e795ebf9f4344322f
HoromiUzumaki <ksushakrotova16@yandex.ru>
2025-03-13 13:36:08 +0300

    Сессия_5

--- WarehouseMob/WarehouseMob/WarehouseMob/obj/Debug/netstandard2.0/WarehouseMob.AssemblyInfo.cs
+++ WarehouseMob/WarehouseMob/WarehouseMob/obj/Debug/netstandard2.0/WarehouseMob.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarehouseMob")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab87840c44a22abf8e956a4f60005b8b25548b0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarehouseMob")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarehouseMob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
